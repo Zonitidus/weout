b41c9c2cc59787aa7ce1198f75705373709c61d2
Zonitidus <zonitidus@gmail.com>
2021-11-30 21:30:07 -0500

    Post event fragment

--- app/src/main/java/edu/co/icesi/weout/activities/HomeActivity.kt
+++ app/src/main/java/edu/co/icesi/weout/activities/HomeActivity.kt
@@ -3,10 +3,13 @@ package edu.co.icesi.weout.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.co.icesi.weout.databinding.ActivityHomeBinding
+import edu.co.icesi.weout.fragments.PostEventFragment
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+
+    val postEventFragment: PostEventFragment = PostEventFragment.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/edu/co/icesi/weout/fragments/PostEventFragment.kt
@@ -0,0 +1,32 @@
+package edu.co.icesi.weout.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import edu.co.icesi.weout.R
+import edu.co.icesi.weout.databinding.FragmentPostEventBinding
+
+class PostEventFragment : Fragment() {
+
+    private var _binding: FragmentPostEventBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPostEventBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        
+
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = PostEventFragment()
+    }
+}
